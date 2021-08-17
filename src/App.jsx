06b17be476cc54e873bcfe755bf0afaf08e0273a
mlhotecky <nerxeus@gmail.com>
2021-08-17 19:44:24 +0200

    updated - App.jsx - add grids for display items properly, implement items and add all necessary logic - first fetch configuration, then fetch loan by useEffect hook, set initFetching bool for ContentWrapper - display loader only one when app is rendered, make callback functions for dummy component, access to them as a props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchConfiguration} from "./redux/actions/configuration";
 import {fetchLoan} from "./redux/actions/loan";
+import Calculator from "./components/Calculator";
+import ContentWrapper from "./components/ContentWrapper";
+import LoanDetail from "./components/LoanDetail";
+import Grid from "@material-ui/core/Grid";
 
 function App() {
 
@@ -11,36 +14,75 @@ function App() {
   const configuration = useSelector(state => state.configuration);
   const loan = useSelector(state => state.loan);
 
+  const {loanStatus, loanObject} = loan;
+  const {configurationStatus, configurationObject} = configuration;
+  const {amountInterval, termInterval} = configurationObject;
+  const initAmount = amountInterval?.defaultValue;
+  const initTerm = termInterval?.defaultValue;
+
+  const [lastAmount, setLastAmount] = useState(0);
+  const [lastTerm, setLastTerm] = useState(0);
+  const [initFetching, setInitFetching] = useState(true);
+
   useEffect(() => {
     dispatch(fetchConfiguration());
-    dispatch(fetchLoan(1000, 5));
-  }, [])
+  }, [dispatch])
 
   useEffect(() => {
-  console.log(configuration);
-  }, [configuration])
+    dispatch(fetchLoan(initAmount,initTerm));
+    setLastAmount(initAmount);
+    setLastTerm(initTerm);
+  }, [configurationObject])
 
   useEffect(() => {
-  console.log(loan);
-  }, [loan])
+    if (initFetching) {
+      setInitFetching(false);
+    }
+  }, [loanObject])
+
+  // const changeAmount = useCallback(amount => () => {
+  //   console.log(amount, lastAmount);
+  //   if (amount !== lastAmount) {
+  //     setLastAmount(amount);
+  //     dispatch(fetchLoan(amount, lastTerm))
+  //   }
+  // }, [])
+  //
+  // const changeTerm = useCallback(term => () => {
+  //   console.log(term, lastTerm)
+  //   if (term !== lastTerm) {
+  //     setLastTerm(term);
+  //     dispatch(fetchLoan(lastAmount, term))
+  //   }
+  // }, [])
+
+  const changeAmount = amount => {
+    if (amount !== lastAmount) {
+      setLastAmount(amount);
+      dispatch(fetchLoan(amount, lastTerm))
+    }
+  };
+
+  const changeTerm = term => {
+    if (term !== lastTerm) {
+      setLastTerm(term);
+      dispatch(fetchLoan(lastAmount, term))
+    }
+  };
 
   return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Grid container justifyContent="center" alignItems="center" direction="column" style={{height: "100vh"}}>
+          <Grid>
+            <ContentWrapper initFetching={initFetching} deps={[loanStatus, configurationStatus]}>
+              <Calculator
+                  changeTerm={changeTerm}
+                  changeAmount={changeAmount}
+                  {...configurationObject}
+              />
+              <LoanDetail {...loanObject} />
+            </ContentWrapper>
+          </Grid>
+        </Grid>
   );
 }
 
