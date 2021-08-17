bd65e36a360de6b20899c096c41fcdf8a67a4431
mlhotecky <nerxeus@gmail.com>
2021-08-17 19:40:24 +0200

    updated - loan, configuration actions - update getStatus functions

--- src/redux/actions/configuration.js
+++ src/redux/actions/configuration.js
@@ -1,30 +1,40 @@
 import {API_PREFIX, ERROR, GET_CONFIGURATION, PENDING, REJECTED, SUCCEEDED} from "../../constants";
 import {getConfiguration, getConfStatus} from "../reducers/configuration";
 
+const getStatus = (dispatch, type, status, message = "") => {
+    dispatch(getConfStatus({
+        type,
+        status,
+        message
+    }))
+}
+
 export const fetchConfiguration = () => dispatch => {
-    dispatch(getConfStatus(GET_CONFIGURATION, PENDING));
+    getStatus(dispatch, GET_CONFIGURATION, PENDING);
     fetch(`${API_PREFIX}/application/constraints`)
         .then(async (response) => {
             const data = await response.json();
 
             if (!response.ok) {
                 const error = (data && data.message) || response.statusText;
-                dispatch(getConfStatus(
+                getStatus(
+                    dispatch,
                     GET_CONFIGURATION,
                     REJECTED,
                     `${ERROR} ${error || ""}`
-                ));
+                );
                 return;
             }
 
-            dispatch(getConfStatus(GET_CONFIGURATION, SUCCEEDED));
+            getStatus(dispatch, GET_CONFIGURATION, SUCCEEDED);
             dispatch(getConfiguration(data));
         })
         .catch(error => {
-            dispatch(getConfStatus(
+            getStatus(
+                dispatch,
                 GET_CONFIGURATION,
                 REJECTED,
                 error?.toString()
-            ));
+            );
         });
 };
--- src/redux/actions/loan.js
+++ src/redux/actions/loan.js
@@ -1,6 +1,14 @@
 import {API_PREFIX, ERROR, GET_LOAN, PENDING, REJECTED, SUCCEEDED} from "../../constants";
 import {getLoan, getLoanStatus} from "../reducers/loan";
 
+const getStatus = (dispatch, type, status, message = "") => {
+    dispatch(getLoanStatus({
+        type,
+        status,
+        message
+    }))
+}
+
 export const fetchLoan = (amount, term) => dispatch => {
 
     const options = {
@@ -8,29 +16,31 @@ export const fetchLoan = (amount, term) => dispatch => {
         term
     }
 
-    dispatch(getLoanStatus(GET_LOAN, PENDING));
+    getStatus(dispatch, GET_LOAN, PENDING);
     fetch(`${API_PREFIX}/application/first-loan-offer?` + new URLSearchParams(options))
         .then(async (response) => {
             const data = await response.json();
 
             if (!response.ok) {
                 const error = (data && data.message) || response.statusText;
-                dispatch(getLoanStatus(
+                getStatus(
+                    dispatch,
                     GET_LOAN,
                     REJECTED,
                     `${ERROR} ${error || ""}`
-                ));
+                );
                 return;
             }
 
-            dispatch(getLoanStatus(GET_LOAN, SUCCEEDED));
+            getStatus(dispatch, GET_LOAN, SUCCEEDED);
             dispatch(getLoan(data));
         })
         .catch(error => {
-            dispatch(getLoanStatus(
+            getStatus(
+                dispatch,
                 GET_LOAN,
                 REJECTED,
                 error?.toString()
-            ));
+            );
         });
 };
